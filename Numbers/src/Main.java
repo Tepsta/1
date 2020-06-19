0054174305525d3a4f490676d795533e4956ed1c
FilipKrumpholc <66184362+FilipKrumpholc@users.noreply.github.com>
2020-06-19 22:03:55 +0200

    Úprava čísel
    Do programu šli zadat pouze kladné čísla, upravil jsem program a zařídil jsem, aby šli zadat i záporné čísla.

--- Numbers/src/Main.java
+++ Numbers/src/Main.java
@@ -7,34 +7,28 @@ public class Main {
 		// TODO Auto-generated method stub
 		Random r = new Random();
 		Scanner s = new Scanner(System.in);
-		System.out.println("What positive number do you want to find?");
+		System.out.println("What number do you want to find?");
 		int targetNumber = s.nextInt();
-		if (targetNumber < 0) {
-			System.err.println("Your target number must be positive\n");
-			System.out.println("What number do you want to find?");
-			targetNumber = s.nextInt();
-		}
-		System.out.println("What is the maximum possible number that can be generated? (shouldn't be bigger than 10000)");
+
+		System.out.println("What is the maximum possible number that can be generated? ");
 		int max = s.nextInt();
-		if (max < targetNumber) {
-			System.err.println("The maximum possible number can not be smaller than your target number");
-			System.out.println("What is the maximum possible number that can be generated? (shouldn't be bigger than 10000)");
-			max = s.nextInt();
-		}
-		int[] number = new int[10000];
-		for (int i = 0; i == i - 0; i++) {
-			number[i] = r.nextInt(max + 1);
-			if (number[i] != targetNumber) {
-				int b = i + 1;
-				System.out.println("generation " + b + ": " + number[i]);
+
+		boolean found = false;
+
+		int gen = 0;
+		while (found == false) {
+			gen++;
+			int temp = (int) (Math.random() * max + 0);
+			System.out.println("generation " + gen + ": " + temp);
+			if (temp == targetNumber) {
+
+				System.out.println("Your number was found in generation " + gen);
+				found = true;
 			}
-			if (number[i] == targetNumber) {
-				int b = i + 1;
-				System.out.println("generation " + b + ": " + number[i]);
-				System.out.println("Your number was found in generation " + b);
-				break;
-			}
+
 		}
 	}
 
 }
+
+
